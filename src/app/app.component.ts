8cb57f973b46ba93cd3edb48efdad71f9182e151
Alain Iglesias <alain.iglesias@umvel.com>
2021-03-18 17:18:45 -0600

    ✨ first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { onAuthUIStateChange, CognitoUserInterface, AuthState } from '@aws-amplify/ui-components';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-amplify-full';
+  title = 'amplify-angular-auth';
+  user: CognitoUserInterface | undefined;
+  authState: AuthState;
+
+  formFields = [
+    {
+      type: "email",
+      label: "Custom email Label",
+      placeholder: "custom email placeholder",
+      required: true,
+    },
+    {
+      type: "password",
+      label: "Custom Password Label",
+      placeholder: "custom password placeholder",
+      required: true,
+    },
+    {
+      type: "phone_number",
+      label: "Custom Phone Label",
+      placeholder: "custom Phone placeholder",
+      required: false,
+    },
+  ];
+
+  constructor(private ref: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      this.ref.detectChanges();
+    })
+  }
+
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
 }
